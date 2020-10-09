e1eb2d5bd416d959c63fffe81fa2ff015a8aee96
AJ Larson <ajlarson@AJsMac.local>
2020-10-09 16:56:51 -0600

    inistial build subtree commi

---
+++ build/precache-manifest.27e710777fa81a8f5c4f0b32b64b2365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ba8b95b3dfa777f806b",
+    "url": "/static/css/main.c54c1f91.chunk.css"
+  },
+  {
+    "revision": "5ba8b95b3dfa777f806b",
+    "url": "/static/js/main.2498bf0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cf7d1eb9c1a32ee9f611",
+    "url": "/static/js/2.d4f5c1c8.chunk.js"
+  },
+  {
+    "revision": "0aa82451dc084483b09d24fda8dfad0d",
+    "url": "/index.html"
+  }
+];
